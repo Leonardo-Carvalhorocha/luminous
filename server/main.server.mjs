5bd4e2a2d4c0072c60f7d4cb60733baaaabb5199
leonardo <leonardorocha.zeta@gmail.com>
2024-07-28 02:06:36 -0300

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-Q636BWHT.mjs";import"./chunk-6VPHJ3UI.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-HHWF6MDF.mjs";import"./chunk-6VPHJ3UI.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
